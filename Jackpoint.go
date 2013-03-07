6d928ef9f509dd35b031afb9a088a82ca4a6554e
bussiere <bussiere@gmail.com>
2013-03-07 10:58:04 +0100

    rajout

--- Jackpoint.go
+++ Jackpoint.go
@@ -3,11 +3,38 @@ package main
 import (
 	"./structure"
 	"fmt"
+	"io"
+	"net/http"
+	"strconv"
 )
+
+func HelloServer(c http.ResponseWriter, req *http.Request) {
+
+	result := "<html><body><form action='/login/' method='post'><table><tr><td><label for='login'><strong>Nom de compte</strong></label></td><td><input type='text' name='login' id='login'/></td></tr><tr><td><label for='pass'><strong>Mot de passe</strong></label></td><td><input type='password' name='pass' id='pass'/></td></tr></table><input type='submit' name='connexion' value='Se connecter'/></form></body></html>"
+	c.Header().Set("Content-Type", "text/html")
+	c.Header().Set("Content-Length", strconv.Itoa(len(result)))
+	io.WriteString(c, result)
+}
+
+func login(c http.ResponseWriter, req *http.Request) {
+	body := r.FormValue("login")
+	fmt.Printf(body)
+	result = ""
+	c.Header().Set("Content-Type", "text/html")
+	c.Header().Set("Content-Length", strconv.Itoa(len(result)))
+
+}
+
+func init() {
+
+}
 
 func main() {
 	j := new(struct_jackpoint.Jack)
 	j.Id = 2
 	fmt.Printf("%d", j.Id)
+	http.HandleFunc("/login/", editHandler)
 
+	http.Handle("/", http.HandlerFunc(HelloServer))
+	http.ListenAndServe(":8050", nil)
 }
--- structure/struct_jackpoint.go
+++ structure/struct_jackpoint.go
@@ -6,38 +6,40 @@ type Jack struct {
 	Updated               int64
 	Nom                   string
 	Email                 string
-	Skill_Jack_id         []rune
-	Carac_Jack_id         []rune
-	Item_Jack_id          []rune
-	Skill_Jack_Private_id []rune
-	Item_Jack_Private_id  []rune
+	Skill_Jack_Id         []rune
+	Carac_Jack_Id         []rune
+	Item_Jack_Id          []rune
+	Skill_Jack_Private_Id []rune
+	Item_Jack_Private_Id  []rune
 	Password              string
 	Key_public            string
 	Key_private           string
-	Message_id            []rune
+	Message_Id            []rune
 	Statut                string
 	Avatar                string
 }
 
 type Admin struct {
-	id      rune
-	Jack_id rune
-	Created int64
-	Updated int64
+	Id          rune
+	Key_public  string
+	Key_private string
+	Jack_Id     rune
+	Created     int64
+	Updated     int64
 }
 
 type Hand struct {
-	id            rune
+	Id            rune
 	Created       int64
 	Updated       int64
-	Skill_Jack_id []rune
-	Carac_Jack_id []rune
-	Item_Jack_id  []rune
+	Skill_Jack_Id []rune
+	Carac_Jack_Id []rune
+	Item_Jack_Id  []rune
 	Message       string
 }
 
 type Skill struct {
-	id          rune
+	Id          rune
 	Created     int64
 	Updated     int64
 	Nom         string
@@ -45,15 +47,15 @@ type Skill struct {
 }
 
 type Filiation_Skill struct {
-	id              rune
+	Id              rune
 	Created         int64
 	Updated         int64
-	Parent_Skill_id rune
-	Enfant_Skill_id rune
+	Parent_Skill_Id rune
+	Enfant_Skill_Id rune
 }
 
 type Carac struct {
-	id          rune
+	Id          rune
 	Created     int64
 	Updated     int64
 	Nom         string
@@ -61,7 +63,7 @@ type Carac struct {
 }
 
 type Item struct {
-	id          rune
+	Id          rune
 	Created     int64
 	Updated     int64
 	Nom         string
@@ -69,39 +71,63 @@ type Item struct {
 }
 
 type Item_Carac struct {
-	id       rune
+	Id       rune
 	Created  int64
 	Updated  int64
-	Carac_id rune
-	Item_id  rune
+	Carac_Id rune
+	Item_Id  rune
 }
 
 type Item_Skill struct {
-	id       rune
+	Id       rune
 	Created  int64
 	Updated  int64
-	Skill_id rune
-	Item_id  rune
+	Skill_Id rune
+	Item_Id  rune
 }
 
 type Skill_Jack struct {
-	id       rune
+	Id       rune
 	Created  int64
 	Updated  int64
-	Skill_id rune
-	Jack_id  rune
+	Skill_Id rune
+	Jack_Id  rune
 }
 type Item_Jack struct {
-	id      rune
+	Id      rune
 	Created int64
 	Updated int64
-	Item_id rune
-	Jack_id rune
+	Item_Id rune
+	Jack_Id rune
 }
 type Carac_Jack struct {
-	id      rune
+	Id      rune
 	Created int64
 	Updated int64
-	Item_id rune
-	Jack_id rune
+	Item_Id rune
+	Jack_Id rune
+}
+
+type Skill_Jack_Private struct {
+	Id       rune
+	Created  int64
+	Updated  int64
+	Skill_Id rune
+	Jack_Id  rune
+}
+type Item_Jack_Private struct {
+	Id      rune
+	Created int64
+	Updated int64
+	Item_Id rune
+	Jack_Id rune
+}
+
+type Admin_Private struct {
+	Id                    rune
+	Admin_Id              rune
+	Id_Item_Jack_Private  Item_Jack_Private
+	Id_Skill_Jack_Private Skill_Jack_Private
+	Created               int64
+	Updated               int64
 }
